6b744745943c672f40308a9e96edce985e57d33b
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-18 17:35:33 +0300

    first commit

---
+++ Instagram Design/customizedCell.swift
@@ -0,0 +1,24 @@
+//
+//  customizedCell.swift
+//  Instagram Design
+//
+//  Created by Atıl Samancıoğlu on 18/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class customizedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
