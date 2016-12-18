83303695af199b1a7143fe013987e8a824daeada
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-18 14:19:57 +0300

    Initial Commit

---
+++ Instagram Design/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Instagram Design
+//
+//  Created by Atıl Samancıoğlu on 18/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Instagram Design/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Instagram Design
+//
+//  Created by Atıl Samancıoğlu on 18/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
